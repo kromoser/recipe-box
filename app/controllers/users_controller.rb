fa070d08d2ff883a3d49bc1fa07863eb0f9e34be
kromoser <kevinromoser@gmail.com>
2016-08-27 10:28:28 -0400

    Created signup route, configured rack-flash.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,20 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do 
-    puts "you have signed up"
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      flash[:message] = "Please fill in all fields"
+      redirect to "/signup"
+    else 
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:id] = @user.id
+      #redirect to "/recipes"
+      puts "you have signed up"
+    end
   end
 
   post '/login' do 
     puts "you are logged in"
   end
+
 
 end
