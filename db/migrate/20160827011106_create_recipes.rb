edf50984aef4dcc61efa5c166dcde2172697c90c
kromoser <kevinromoser@gmail.com>
2016-08-26 21:18:05 -0400

    Wrote migration to set up recipes table.

--- db/migrate/20160827011106_create_recipes.rb
+++ db/migrate/20160827011106_create_recipes.rb
@@ -1,4 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :link
+      t.string :cooking_time
+    end
   end
 end
