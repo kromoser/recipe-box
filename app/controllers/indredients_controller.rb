18102be07ecd22551f0585503493afbf97679c8d
kromoser <kevinromoser@gmail.com>
2016-08-29 23:05:06 -0400

    Started building ingredient show view and added link from each ingredient in ingredient index page

--- app/controllers/indredients_controller.rb
+++ app/controllers/indredients_controller.rb
@@ -4,10 +4,14 @@ class IngredientsController < ApplicationController
     confirm_logged_in
     @user = current_user
     @ingredients = all_users_ingredients(current_user).flatten
-
     erb :'/ingredients/index'
   end
 
+  get '/ingredients/:id' do
+    confirm_logged_in
+    @ingredient = Ingredient.find(params[:id])
+    erb :'/ingredients/show'
+  end 
 
 
 end
