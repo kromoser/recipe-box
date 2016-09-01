db682815599f452b69b954bdc0848d5a357d2328
kromoser <kevinromoser@gmail.com>
2016-09-01 12:24:49 -0400

    Added routes to TypesController, linked to each recipe category's show page on category index page

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,8 +1,20 @@
 class TypesController < ApplicationController
 
   get '/categories' do 
+    confirm_logged_in
+    @user = current_user
     @categories = users_types(current_user).flatten
     erb :'/categories/index'
   end
+
+  get '/categories/:id' do
+    confirm_logged_in
+    @category = Type.find(id: params[:id])
+    @recipes = @category.recipes.select do |recipe|
+      recipe.user == current_user
+    end 
+
+    erb :'/categories/show'
+
 
 end
